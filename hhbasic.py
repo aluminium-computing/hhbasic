c2724c5629e4c6ce2297938f47ac1b89ad1f5ac1
Mark Larson <aluminium-mal>
2014-09-13 12:49:45 -0700

    Implement parser checking for valid commands or program statements.

--- hhbasic.py
+++ hhbasic.py
@@ -10,11 +10,30 @@ def HHBasic():
   os.system('clear') # clear screen
   print " HHBasic pa1.0"
   print " Copyright 2014 Aluminium Computing, Inc."
+  interpreter_commands = [
+    "HALT",   # exit
+    "LIST",   # list program in memory
+    "LOAD",   # load a program from disk
+    "NEW",    # clear memory
+    "RUN",    # execute the program in memory
+    "SAVE",   # save program in memory to disk 
+  ]
   while True:
-    cmd = raw_input(">")
-    if cmd == "HALT": exit()
+    inline = raw_input(">")
+    cmd = inline.split(" ")
+    is_statement = False
+    if cmd[0] == "HALT": exit()
+    try:
+      is_statement = int(cmd[0])
+    except ValueError:
+      if cmd[0] not in interpreter_commands:
+        print("I don't understand what you're trying to do. That's not a command or a program line.")
+        continue
+    # At this point we either have a statement with a valid line number or a command to process
+
 
 def main():
+  # TODO: Trap for KeyboardInterrupt exception to handle Ctrl-C
   HHBasic()
 
 if __name__ == "__main__": main()
